a934081110242033b1541872dfcb5c4b8f3d395a
vyeung <vl.yeung@yahoo.com>
2019-04-20 11:38:50 -0400

    add @LoadBalanced in Orchestrator main

--- src/main/java/com/st/orchestrator/OrchestratorApplication.java
+++ src/main/java/com/st/orchestrator/OrchestratorApplication.java
@@ -2,6 +2,7 @@ package com.st.orchestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,5 +14,6 @@ public class OrchestratorApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() { return new RestTemplate();}
 }
